0e173087dd86b6b0f52893358c75dc0969bdebc1
Eredost <michael.coutin62@gmail.com>
2017-12-11 08:49:05 +0100

    Modification du selecteur CSS

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     
-    $("a:first").click(function(){
+    $(".button1").click(function(){
         alert("Le CV papier n'est pas encore disponible !");
     });
     
