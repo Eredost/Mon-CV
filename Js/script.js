3f486847abfb02bd00d0c9a932a947b92d05b5f1
Eredost <michael.coutin62@gmail.com>
2017-12-11 09:36:38 +0100

    Ajout d'une animation fluide pour la 'Navigation Bar'

--- Js/script.js
+++ Js/script.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
         alert("Le CV papier n'est pas encore disponible !");
     });
     
+   $(".navbar a, footer a").on("click", function(event){
+       
+      event.preventDefault();
+      var hash = this.hash;
+      
+      $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+   });
+
+
 });
